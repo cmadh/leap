f341d820409205ac1db4fb3cd3e6045b9bb06ca8
corvinmadh <grizzlycorv@googlemail.com>
2022-09-10 21:16:04 +0200

    made deep_mind_handler inherit from deep_mind_handler_base to be able to add and inject different deep_mind_handlers to allow replacement of stdout-based logging with swig-directors

--- libraries/chain/include/eosio/chain/deep_mind.hpp
+++ libraries/chain/include/eosio/chain/deep_mind.hpp
@@ -42,7 +42,65 @@ public:
    {}
 };
 
-class deep_mind_handler
+class deep_mind_handler_base
+{
+public:
+    virtual ~deep_mind_handler_base(){}
+    struct deep_mind_config {
+        bool zero_elapsed = false; // if true, the elapsed field of transaction and action traces is always set to 0 (for reproducibility)
+    };
+
+    virtual void update_config(deep_mind_config config);
+
+    virtual void update_logger(const std::string& logger_name);
+    enum class operation_qualifier { none, modify, push };
+
+    virtual void on_startup(chainbase::database& db, uint32_t head_block_num);
+    virtual void on_start_block(uint32_t block_num);
+    virtual void on_accepted_block(const std::shared_ptr<block_state>& bsp);
+    virtual void on_switch_forks(const block_id_type& old_head, const block_id_type& new_head);
+    virtual void on_onerror(const signed_transaction& etrx);
+    virtual void on_onblock(const signed_transaction& trx);
+    virtual void on_start_transaction();
+    virtual void on_end_transaction();
+    virtual void on_applied_transaction(uint32_t block_num, const std::shared_ptr<transaction_trace>& trace);
+    virtual void on_add_ram_correction(const account_ram_correction_object& rco, uint64_t delta);
+    virtual void on_preactivate_feature(const protocol_feature& feature);
+    virtual void on_activate_feature(const protocol_feature& feature);
+    virtual void on_input_action();
+    virtual void on_end_action();
+    virtual void on_require_recipient();
+    virtual void on_send_inline();
+    virtual void on_send_context_free_inline();
+    virtual void on_cancel_deferred(operation_qualifier qual, const generated_transaction_object& gto);
+    virtual void on_send_deferred(operation_qualifier qual, const generated_transaction_object& gto);
+    virtual void on_create_deferred(operation_qualifier qual, const generated_transaction_object& gto, const packed_transaction& packed_trx);
+    virtual void on_fail_deferred();
+    virtual void on_create_table(const table_id_object& tid);
+    virtual void on_remove_table(const table_id_object& tid);
+    virtual void on_db_store_i64(const table_id_object& tid, const key_value_object& kvo);
+    virtual void on_db_update_i64(const table_id_object& tid, const key_value_object& kvo, account_name payer, const char* buffer, std::size_t buffer_size);
+    virtual void on_db_remove_i64(const table_id_object& tid, const key_value_object& kvo);
+    virtual void on_init_resource_limits(const resource_limits::resource_limits_config_object& config, const resource_limits::resource_limits_state_object& state);
+    virtual void on_update_resource_limits_config(const resource_limits::resource_limits_config_object& config);
+    virtual void on_update_resource_limits_state(const resource_limits::resource_limits_state_object& state);
+    virtual void on_newaccount_resource_limits(const resource_limits::resource_limits_object& limits, const resource_limits::resource_usage_object& usage);
+    virtual void on_update_account_usage(const resource_limits::resource_usage_object& usage);
+    virtual void on_set_account_limits(const resource_limits::resource_limits_object& limits);
+    // The trace is consumed by the next ram_event or ram_correction
+    virtual void on_ram_trace(std::string&& event_id, const char* family, const char* operation, const char* legacy_tag);
+    virtual void on_ram_event(account_name account, uint64_t new_usage, int64_t delta);
+    virtual void on_create_permission(const permission_object& p);
+    virtual void on_modify_permission(const permission_object& old_permission, const permission_object& new_permission);
+    virtual void on_remove_permission(const permission_object& permission);
+private:
+    uint32_t         _action_id = 0;
+    ram_trace        _ram_trace;
+    deep_mind_config _config;
+    fc::logger       _logger;
+};
+
+class deep_mind_handler : public deep_mind_handler_base
 {
 public:
    struct deep_mind_config {
@@ -52,7 +110,6 @@ public:
    void update_config(deep_mind_config config);
 
    void update_logger(const std::string& logger_name);
-   enum class operation_qualifier { none, modify, push };
 
    void on_startup(chainbase::database& db, uint32_t head_block_num);
    void on_start_block(uint32_t block_num);
--- libraries/chain/include/eosio/chain/resource_limits.hpp
+++ libraries/chain/include/eosio/chain/resource_limits.hpp
@@ -9,7 +9,7 @@
 
 namespace eosio { namespace chain {
 
-   class deep_mind_handler;
+   class deep_mind_handler_base;
 
    namespace resource_limits {
    namespace impl {
@@ -61,7 +61,7 @@ namespace eosio { namespace chain {
    class resource_limits_manager {
       public:
 
-         explicit resource_limits_manager(chainbase::database& db, std::function<deep_mind_handler*()> get_deep_mind_logger)
+         explicit resource_limits_manager(chainbase::database& db, std::function<deep_mind_handler_base*()> get_deep_mind_logger)
          :_db(db),_get_deep_mind_logger(get_deep_mind_logger)
          {
          }
@@ -109,7 +109,7 @@ namespace eosio { namespace chain {
 
       private:
          chainbase::database&         _db;
-         std::function<deep_mind_handler*()> _get_deep_mind_logger;
+         std::function<deep_mind_handler_base*()> _get_deep_mind_logger;
    };
 } } } /// eosio::chain
 
