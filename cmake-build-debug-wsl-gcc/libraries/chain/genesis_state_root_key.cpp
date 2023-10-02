22d2ce572b02411d897cd6494a19004047b3202b
cmadh <39434158+cmadh@users.noreply.github.com>
2023-10-02 16:12:23 +0200

    Delete cmake-build-debug-wsl-gcc directory

--- cmake-build-debug-wsl-gcc/libraries/chain/genesis_state_root_key.cpp
+++
@@ -1,7 +0,0 @@
-#include <eosio/chain/genesis_state.hpp>
-
-namespace eosio { namespace chain {
-
-const string genesis_state::eosio_root_key = "EOS6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV";
-
-} } // namespace eosio::chain
--- cmake-build-debug-wsl-gcc/unittests/include/deep-mind.hpp
+++
@@ -1,1 +0,0 @@
-#define DEEP_MIND_LOGFILE "/mnt/e/source/repos/leap-experiments/cmadhLeap2/unittests/deep-mind/deep-mind.log"
